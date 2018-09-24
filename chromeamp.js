1d94296987b8fa84a6996e206f8212d5a8f8aa6c
Hafid Maulana <maulanahafid@gmail.com>
2018-09-24 18:55:36 +0700

    Update chromeamp.js

--- chromeamp.js
+++ chromeamp.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
- if ($("#chrome").attr("href") != "https://www.amp-blogger.com/") {
+ if ($("#crum").attr("href") != "https://www.amp-blogger.com/") {
   window.location.href = "https://www.amp-blogger.com/";
  }
 });
