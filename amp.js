92b39ca61b82f0954c18733b20384bbbb53d4592
Hafid Maulana <maulanahafid@gmail.com>
2018-09-25 13:54:19 +0700

    Update and rename crj.js to amp.js

--- amp.js
+++ amp.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
  if ($("#crjxx").attr("href") != "https://www.amp-blogger.com/") {
   window.location.href = "https://www.amp-blogger.com/";
  }
+if (!document.getElementById("alpha")) {
+    window.location.href = "https://www.amp-blogger.com";
+}
 });
