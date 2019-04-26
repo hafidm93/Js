cc4835c7eed3e86f3ae820927aa5ed8db234b08a
Hafid Maulana <maulanahafid@gmail.com>
2019-04-27 03:09:44 +0700

    Update amp-script.js

--- amp-script.js
+++ amp-script.js
@@ -1,1 +1,8 @@
-
+// hello-world.js
+const button = document.getElementById('hello');
+button.addEventListener('click', () => {
+  const el = document.createElement('h1');
+  el.textContent = 'Hello World!';
+  // `document.body` is effectively the <amp-script> element.
+  document.body.appendChild(el);
+});
