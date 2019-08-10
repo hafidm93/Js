911c7fa3463159f1b27178cf03c579874b205a1a
Hafid Maulana <maulanahafid@gmail.com>
2019-08-10 23:51:11 +0700

    Update toc-blogger.js

--- toc-blogger.js
+++ toc-blogger.js
@@ -1,1 +1,35 @@
+window.onload = function () {
+    var toc = "";
+    var level = 0;
 
+    document.querySelector(".post-body").innerHTML =
+        document.querySelector(".post-body").innerHTML.replace(
+            /<h([\d])>([^<]+)<\/h([\d])>/gi,
+            function (str, openLevel, titleText, closeLevel) {
+                if (openLevel != closeLevel) {
+                    return str;
+                }
+
+                if (openLevel > level) {
+                    toc += (new Array(openLevel - level + 1)).join("<ul>");
+                } else if (openLevel < level) {
+                    toc += (new Array(level - openLevel + 1)).join("</ul>");
+                }
+
+                level = parseInt(openLevel);
+
+                var anchor = titleText.replace(/ /g, "_");
+                toc += "<li><a href=\"#" + anchor + "\">" + titleText
+                    + "</a></li>";
+
+                return "<h" + openLevel + " id=" + anchor + ">"
+                    + titleText + "</h" + closeLevel + ">";
+            }
+        );
+
+    if (level) {
+        toc += (new Array(level + 1)).join("</ul>");
+    }
+
+    document.getElementById("toc").innerHTML += toc;
+};
